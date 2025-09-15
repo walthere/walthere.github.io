a70b71cb9a972aa2381ec8d0c46762b43b91d093
walthere <walthere@vip.qq.com>
2025-09-15 15:37:41 +0800

    updated

--- app/pages/posts/[id].vue
+++ app/pages/posts/[id].vue
@@ -1,28 +1,24 @@
 <template>
   <div class="markdown-body p-10">
-   
-  <ContentRenderer :value="page" />
+    <ContentRenderer :value="page" />
   </div>
 </template>
 
 <script lang="ts" setup>
-const route = useRoute()
+const route = useRoute();
 
-console.info('route',route.path)
+console.info("route", route.path);
 
 const { data: page } = await useAsyncData(route.path, () => {
-  return queryCollection('post').path(route.path).first()
-})
+  return queryCollection("post").path(route.path).first();
+});
 
-console.info(page.value)
+console.info(page.value);
 
 useSeoMeta({
   title: page.value?.title,
   ogTitle: page.value?.title,
-  // description: 'This is my amazing site, let me tell you all about it.',
-  // ogDescription: 'This is my amazing site, let me tell you all about it.',
-  // ogImage: 'https://example.com/image.png',
-  // twitterCard: 'summary_large_image',
-})
-
+  description: page.value?.description,
+  ogDescription: page.value?.description,
+});
 </script>
---
+++ content/posts/linuxmirrors.md
@@ -0,0 +1,21 @@
+---
+title: '使用 LinuxMirrors 更换 Linux 和 docker 源'
+description: 'GNU/Linux 更换系统软件源脚本及 Docker 安装与换源脚本,一键脚本使换源更简单'
+date: '2025-09-15'
+hot: true
+tag: 'linux'
+---
+
+# 使用 LinuxMirrors 更换 Linux 和 docker 源
+
+> 来源网站 https://linuxmirrors.cn/
+
+```shell
+# for linux
+bash <(curl -sSL https://linuxmirrors.cn/main.sh)
+
+# for docker
+bash <(curl -sSL https://linuxmirrors.cn/docker.sh)
+```
+
+
